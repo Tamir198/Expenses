88f6a6f44b2daf2fc4b7880246d8408b37ea9dbe
Tamir <1tamir198@gmail.com>
2021-07-07 16:52:32 +0300

    Adding use state hook  in expenses item

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,27 @@
-import './ExpenseItem.css'
-import ExpenseDate from'./ExpenseDate'
-import Card from '../UI/Card'
+import { useState } from 'react';
+import Card from '../UI/Card';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
 
 const ExpenseItem = (props) => {
+    //Use state return an array 
+    // first value is title, second value a function 
+    const [title, setTitle] = useState(props.title);
+
+
+    const clickHandle = () => {
+        setTitle('Updated');
+        console.log(title);
+    }
+
     return (
         <Card className="expense-item">
-          <ExpenseDate date={props.date}/>
+            <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
+                <button onClick={clickHandle}></button>
             </div>
         </Card>
     );
