c808645816c6ec3a92c7fe5647b78a066ab7dd8d
Tamir <1tamir198@gmail.com>
2021-07-11 14:50:27 +0300

    Adding DropDown menu so select year +
    In addition, dead code was removed

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,23 +5,12 @@ import './ExpenseItem.css';
 
 
 const ExpenseItem = (props) => {
-    //Use state return an array 
-    // first value is title, second value a function 
-    const [title, setTitle] = useState(props.title);
-
-
-    const clickHandle = () => {
-        setTitle('Updated');
-        console.log(title);
-    }
-
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
-                <button onClick={clickHandle}></button>
             </div>
         </Card>
     );
