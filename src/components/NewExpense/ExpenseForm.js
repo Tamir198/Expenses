6da2277f0a97470cc6063454e40a655a5f0113cf
Tamir <1tamir198@gmail.com>
2021-07-12 21:36:27 +0300

    Adding the option to cancel expense

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -56,6 +56,7 @@ const ExpenseForm = (props) => {
 
         <div className="new-expense__actions">
             <button type="submit">Add Expense</button>
+            <button type="button" onClick={props.onCancle}>Cancle</button>
         </div>
     </form>
 }
